aa3062c41056c678375f7129ddee2450c9bebe89
Shehroz Raxmatov <shehrozraxmatov@Shehrozs-MacBook-Air.local>
2023-02-27 21:09:33 +0500

    reset

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require("events")
+const express = require("express")
 const app = express()
 
-PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
-app.listen(PORT, ()=>{
+app.listen(3000, ()=>{
     console.log(`connect server ${PORT}`);
 })
