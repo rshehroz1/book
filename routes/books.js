acc60b9a9c8950187fae407a4c7a26ed5f6cc4a9
Shehroz <116956168+rshehroz1@users.noreply.github.com>
2023-03-10 21:10:58 +0500

    data add

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,10 @@
 const { Router } = require("express")
+const Book = require("../models/book")
 const router = Router()
 
-router.get("/", (req, res) =>{
-    res.render("books", {title: "Books", isBooks: true})
+router.get("/", async (req, res) =>{
+    const books = await Book.getAll()
+    res.render("books", {title: "Books", isBooks: true, books})
 })
 
 module.exports = router
